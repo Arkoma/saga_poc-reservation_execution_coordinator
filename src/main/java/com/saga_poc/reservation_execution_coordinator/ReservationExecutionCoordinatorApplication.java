b9003eb41318c44b868b5378450d8e270cffd781
Aaron Burk <aburk@veracode.com>
2022-02-18 15:44:27 -0500

    initial commit

---
+++ src/main/java/com/saga_poc/reservation_execution_coordinator/ReservationExecutionCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.saga_poc.reservation_execution_coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationExecutionCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationExecutionCoordinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saga_poc/reservation_execution_coordinator/ReservationExecutionCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga_poc.reservation_execution_coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationExecutionCoordinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
