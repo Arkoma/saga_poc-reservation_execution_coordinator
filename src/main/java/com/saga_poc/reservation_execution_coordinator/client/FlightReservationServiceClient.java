c41dc626526c68bcdfc4ef99bea9784357a957a4
Aaron Burk <aburk@veracode.com>
2022-06-03 17:03:07 -0400

    remove unnecessary imports

--- src/main/java/com/saga_poc/reservation_execution_coordinator/client/FlightReservationServiceClient.java
+++ src/main/java/com/saga_poc/reservation_execution_coordinator/client/FlightReservationServiceClient.java
@@ -3,8 +3,6 @@ package com.saga_poc.reservation_execution_coordinator.client;
 import com.saga_poc.reservation_execution_coordinator.model.Endpoints;
 import com.saga_poc.reservation_execution_coordinator.model.FlightReservation;
 import com.saga_poc.reservation_execution_coordinator.model.FlightReservationResponse;
-import com.saga_poc.reservation_execution_coordinator.model.HotelReservation;
-import com.saga_poc.reservation_execution_coordinator.model.HotelReservationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- src/main/java/com/saga_poc/reservation_execution_coordinator/client/FlightReservationServiceFallback.java
+++ src/main/java/com/saga_poc/reservation_execution_coordinator/client/FlightReservationServiceFallback.java
@@ -2,8 +2,6 @@ package com.saga_poc.reservation_execution_coordinator.client;
 
 import com.saga_poc.reservation_execution_coordinator.model.FlightReservation;
 import com.saga_poc.reservation_execution_coordinator.model.FlightReservationResponse;
-import com.saga_poc.reservation_execution_coordinator.model.StatusEnum;
-import com.saga_poc.reservation_execution_coordinator.service.ReservationStepCoordinator;
 import org.springframework.stereotype.Component;
 
 import static com.saga_poc.reservation_execution_coordinator.model.StatusEnum.CANCELED;
@@ -11,16 +9,10 @@ import static com.saga_poc.reservation_execution_coordinator.model.StatusEnum.CA
 @Component
 public class FlightReservationServiceFallback implements FlightReservationServiceClient {
 
-    private ReservationStepCoordinator reservationStepCoordinator;
-
-    public FlightReservationServiceFallback (ReservationStepCoordinator reservationStepCoordinator) {
-        this.reservationStepCoordinator = reservationStepCoordinator;
-    }
-
     @Override
     public FlightReservationResponse makeReservation(FlightReservation flightReservation) {
-       final FlightReservationResponse response = new FlightReservationResponse();
-       response.setStatus(CANCELED);
-       return response;
+        final FlightReservationResponse response = new FlightReservationResponse();
+        response.setStatus(CANCELED);
+        return response;
     }
 }
